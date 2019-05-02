b3aba82617d9355dea9a7e0cdabbdf2fb96f521e
spd1238@gmail.com <spd1238@gmail.com>
2019-05-02 12:28:27 +0530

    Added a few more classes for Matrices Class including generating Identity Matrices, Matrix Multiplications and Exponentiation with Divide and Conquer

--- src/com/clrs/matricesHelper/Matrices.java
+++ src/com/clrs/matricesHelper/Matrices.java
@@ -1,5 +1,7 @@
 package com.clrs.matricesHelper;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Matrices {
     private int[][] matrix;
 
@@ -30,6 +32,63 @@ public class Matrices {
                 j++;
             }
             i++;
+        }
+    }
+    public int getNumberOfRows(){
+        return matrix.length;
+    }
+
+    public  int getNumberOfColumns(){
+        return matrix[0].length;
+    }
+
+    public Matrices multiplication (@NotNull Matrices matrixB) throws Exception {
+        int m = this.getNumberOfRows();
+        int n = this.getNumberOfColumns();
+        int p = matrixB.getNumberOfRows();
+        int q = matrixB.getNumberOfColumns();
+        if (n == p){
+            int [][] res = new int[m][q];
+            for(int i = 0; i<m; i++)
+                for (int j = 0; j < q; j++) {
+                    for (int k = 0; k < p; k++) {
+                        res[i][j] += (this.matrix[i][k] * matrixB.matrix[k][j]);
+                    }
+                }
+            return new Matrices(res);
+        }
+        else{
+            throw new Exception("Number of Columns not equal to Number of Rows");
+        }
+    }
+
+    public boolean checkSquare(){
+        return this.matrix.length == this.matrix[0].length;
+    }
+    public Matrices generateID(){
+        int len = this.matrix.length;
+        int [][]res = new int[len][len];
+        for (int i = 0; i <len ; i++) {
+            for (int j = 0; j <len ; j++) {
+                res[i][j] = (i==j)?1:0;
+            }
+        }
+        return new Matrices(res);
+    }
+    public Matrices exponentiation(int num) throws Exception {
+        if (checkSquare()) {
+            Matrices result = generateID();
+            while (num > 0) {
+                if (num % 2 == 0)
+                    result = result.multiplication(this);
+
+                this.matrix = this.multiplication(this).matrix;
+                num = num / 2;
+            }
+            return result;
+        }
+        else{
+            throw new Exception("Matrix should be a square Matrix for Exponentiation");
         }
     }
     public String toString(){
--- src/com/clrs/matricesHelper/MatrixMain.java
+++ src/com/clrs/matricesHelper/MatrixMain.java
@@ -1,8 +1,17 @@
 package com.clrs.matricesHelper;
 
 public class MatrixMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Matrices m = new Matrices("2 2 2\n3 3 3\n4 4 4\n");
+
         System.out.println(m);
+
+
+        try {
+            System.out.println(m.exponentiation(10));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
